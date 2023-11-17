a547483bac87e52e41c1a4e1a08c67df6060b868
mehoppe <mhoppe@Mikes-MBP.attlocal.net>
2023-11-16 18:36:35 -0600

    build collection

---
+++ weaviate.py
@@ -0,0 +1,81 @@
+import json
+import logging
+import os
+import requests
+import weaviate
+
+
+def connect(weaviate_key, openai_api_key, log):
+    try:
+        client = weaviate.Client(
+            url="https://weaviate-64pq3jgx.weaviate.network",
+            auth_client_secret=weaviate.AuthApiKey(api_key=weaviate_key),
+            additional_headers = {
+                "X-OpenAI-Api-Key": openai_api_key
+            }
+        )
+        return client 
+    except:    
+        test = client.schema.get()
+        log.error("Connection to Weaviate fail: %s", test)
+
+def build_class(client, class_obj, log):
+    try:
+        test_schema = client.schema.create_class(class_obj)
+        log.info("Class obj created.")
+    except:
+        log.warning("Class obj already exists.")
+
+def add_objs(client, log):
+    resp = requests.get('https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json')
+    data = json.loads(resp.text)  # Load data
+
+    client.batch.configure(batch_size=100)  # Configure batch
+    with client.batch as batch:  # Initialize a batch process
+        for i, d in enumerate(data):  # Batch import data
+            log.debug(f"importing question: {i+1}")
+            properties = {
+                "answer": d["Answer"],
+                "question": d["Question"],
+                "category": d["Category"],
+            }
+            try:
+                batch.add_data_object(
+                    data_object=properties,
+                    class_name="Question"
+                )
+            except:
+                log.error("OpenAI API Failure.")
+        log.debug("All objects added.")
+
+def main():
+    log = logging.getLogger("app")
+    logging.basicConfig()
+    log.setLevel(logging.WARNING)
+
+    try:
+        weaviate_key = os.environ["weaviate_key"]
+        openai_api_key = os.environ["openai_api_key"]
+        log.info("Environment variables loaded.")
+    except:
+        log.error("Environment variables NOT loaded.")
+
+    class_obj = {
+        "class": "Question",
+        "vectorizer": "text2vec-openai",  # If set to "none" you must always provide vectors yourself. Could be any other "text2vec-*" also.
+        "moduleConfig": {
+            "text2vec-openai": {},
+            "generative-openai": {}  # Ensure the `generative-openai` module is used for generative queries
+        }
+    }
+    
+    client = connect(weaviate_key, openai_api_key, log)
+    log.info("Connected to Weaviate.")
+    build_class(client, class_obj, log)
+    log.info("Class Object created.")
+    add_objs(client, log)
+    log.info("Objects added to class/collection.")
+
+
+if __name__ == "__main__":
+    main()
